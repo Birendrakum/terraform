295357b3673d83b631363cdcec9c003d3effab6f
Birendrakum <84340734+Birendrakum@users.noreply.github.com>
2022-08-05 17:45:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "aws_instance" "myFirstInstance" {
   connection {
     host = self.public_ip
     type = "ssh"
-    user = "ec2_user"
+    user = "corestack-cb50e"
     private_key = var.SSH_key
 }
  provisioner "local-exec" {
