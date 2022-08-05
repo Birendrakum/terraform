9052183e77408e31f1a8c14a4c92187b99258d8e
Birendrakum <84340734+Birendrakum@users.noreply.github.com>
2022-08-05 23:10:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ resource "aws_instance" "myFirstInstance" {
     private_key = file(var.SSH_key)
 }
  provisioner "local-exec" {
-   command = "ansible-playbook -i ${aws_instance.myFirstInstance.public_ip}, --private-key ${var.SSH_key} play.yml "
+   command = "ansible-playbook -i ${aws_instance.myFirstInstance.public_ip}, --private-key file(var.SSH_key) play.yml "
  }
 }
 
